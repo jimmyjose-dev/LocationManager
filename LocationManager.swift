4761a4706f70b4936dd72105e435a5fc4b56ab87
Varshyl Mobile <jimmy@varshyl.com>
2014-08-17 03:52:22 +0530

    First commit

---
+++ LocationManager.swift
@@ -0,0 +1,109 @@
+//
+//  LocationManager.swift
+//
+//
+//  Created by Jimmy Jose on 14/08/14.
+//  Copyright (c) 2014 Varshyl Mobile Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class LocationManager: NSObject,CLLocationManagerDelegate {
+    
+    
+    var delegate:LocationManagerDelegate? = nil
+    
+    var latitude:String = ""
+    var longitude:String = ""
+    
+    var locationManager: CLLocationManager!
+    
+    class var sharedInstance : LocationManager {
+    struct Static {
+        static let instance : LocationManager = LocationManager()
+        }
+        return Static.instance
+    }
+    
+    
+    override init(){
+        
+        super.init()
+        
+    }
+    
+    func start(){
+        
+        initLocationManager()
+    }
+    
+    func initLocationManager() {
+        
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        // locationManager.locationServicesEnabled
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        //locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        locationManager.stopUpdatingLocation()
+        if delegate? != nil{
+            delegate?.locationManagerHasError(error.localizedDescription)
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        var arrayOfLocation = locations as NSArray
+        var location = arrayOfLocation.lastObject as CLLocation
+        var coordLatLon = location.coordinate
+        if delegate? != nil{
+            delegate?.locationFound(coordLatLon.latitude.description,longitude: coordLatLon.longitude.description)
+        }
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!,
+        didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+            var hasAuthorised = false
+            var locationStatus : NSString = "Calibrating"
+            switch status {
+            case CLAuthorizationStatus.Restricted:
+                locationStatus = "Restricted Access"
+            case CLAuthorizationStatus.Denied:
+                locationStatus = "Denied access"
+            case CLAuthorizationStatus.NotDetermined:
+                locationStatus = "Not determined"
+            default:
+                locationStatus = "Allowed access"
+                hasAuthorised = true
+            }
+            
+            if (hasAuthorised == true) {
+                locationManager.startUpdatingLocation()
+            }else{
+                
+                if (delegate? != nil){
+                    delegate?.locationManagerStatus(locationStatus)
+                    
+                }
+            }
+            
+    }
+}
+
+
+@objc protocol LocationManagerDelegate : NSObjectProtocol
+{
+    func locationFound(latitude:NSString, longitude:NSString)
+    func locationManagerStatus(status:NSString)
+    func locationManagerHasError(error:NSString)
+}
+
---
+++ README.md
@@ -0,0 +1,28 @@
+LocationManager
+=====================
+
+CLLocationManager wrapper written in Swift
+
+Three helpful delegates in LocationManager
+
+    func locationFound(latitude:NSString, longitude:NSString)
+    func locationManagerStatus(status:NSString)
+    func locationManagerHasError(error:NSString)
+
+----------
+
+Add Privacy - Location Usage Description  and NSLocationWhenInUseUsageDescription in your plist
+
+
+Roadmap
+---------------
+
+Compatiblity check for iOS7 & iOS8
+
+----------
+Contact Us
+---------------
+
+Have any questions or suggestions feel free to write at jimmy@varshyl.com (Jimmy Jose)
+http://www.varshylmobile.com/
+
